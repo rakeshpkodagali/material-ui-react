f95bac050defff2e7f1e244a54347f42e2c69719
Rakesh Kodagali <rpk.300@gmail.com>
2021-11-22 21:30:18 +0530

    create routes for app.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import React from "react";
+import Notes from "./pages/Notes";
+import Create from "./pages/Create";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Notes />
+        </Route>
+        <Route path="/create">
+          <Create />
+        </Route>
+      </Switch>
+    </Router>
+  );
 };
 
 export default App;
