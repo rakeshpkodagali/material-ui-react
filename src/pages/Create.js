a91bc49451e0cac74ece0bfd52b6cb76fb3a5e3a
Rakesh Kodagali <rpk.300@gmail.com>
2021-11-22 22:04:37 +0530

    add Typography component.

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,7 +1,18 @@
 import React from "react";
+import Typography from "@material-ui/core/Typography";
 
 const Create = () => {
-  return <div>Create Page</div>;
+  return (
+    <div>
+      <Typography
+        variant="h6"
+        component="h2"
+        color="textSecondary"
+        gutterBottom
+        children="Create a New Note."
+      />
+    </div>
+  );
 };
 
 export default Create;
