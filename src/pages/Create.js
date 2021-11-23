b341b980df3a63cef7c3a904d75c329ad07f915e
Rakesh Kodagali <rpk.300@gmail.com>
2021-11-24 00:19:47 +0530

    add Material Icons.

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -2,6 +2,7 @@ import React from "react";
 import Button from "@material-ui/core/Button";
 import Container from "@material-ui/core/Container";
 import Typography from "@material-ui/core/Typography";
+import KeyboardArrowRightIcon from "@material-ui/icons/KeyboardArrowRight";
 
 const Create = () => {
   return (
@@ -13,7 +14,12 @@ const Create = () => {
         gutterBottom
         children="Create a New Note."
       />
-      <Button color="secondary" variant="contained" type="submit">
+      <Button
+        color="secondary"
+        variant="contained"
+        type="submit"
+        endIcon={<KeyboardArrowRightIcon />}
+      >
         Submit
       </Button>
     </Container>
