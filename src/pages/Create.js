ff9c5decbff72ec8d2cbe3dd423e3da79f5a9d0f
Rakesh Kodagali <rpk.300@gmail.com>
2021-11-27 21:10:25 +0530

    Work with mui form textfields.

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,14 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "@material-ui/core/Button";
 import Container from "@material-ui/core/Container";
 import Typography from "@material-ui/core/Typography";
 import KeyboardArrowRightIcon from "@material-ui/icons/KeyboardArrowRight";
+import { TextField } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core";
 
-const useStyles = makeStyles({});
+const useStyles = makeStyles({
+  field: {
+    margin: "20px 0",
+  },
+});
 
 const Create = () => {
+  const [title, setTitle] = useState("");
+  const [details, setDetails] = useState("");
+  const [titleError, setTitleError] = useState(false);
+  const [detailsError, setDetailsError] = useState(false);
   const classes = useStyles();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setTitleError(false);
+    setDetailsError(false);
+
+    if (!title) setTitleError(true);
+    if (!details) setDetailsError(true);
+
+    if (title && details) console.log(title, details);
+  };
 
   return (
     <Container maxWidth="xl">
@@ -19,14 +39,40 @@ const Create = () => {
         gutterBottom
         children="Create a New Note."
       />
-      <Button
-        color="secondary"
-        variant="contained"
-        type="submit"
-        endIcon={<KeyboardArrowRightIcon />}
-      >
-        Submit
-      </Button>
+      <form noValidate autoComplete="off" onSubmit={handleSubmit}>
+        <TextField
+          className={classes.field}
+          label="Note Title"
+          variant="outlined"
+          color="secondary"
+          fullWidth
+          required
+          value={title}
+          error={titleError}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <TextField
+          className={classes.field}
+          label="Note Details"
+          variant="outlined"
+          color="secondary"
+          multiline
+          rows={4}
+          fullWidth
+          required
+          value={details}
+          error={detailsError}
+          onChange={(e) => setDetails(e.target.value)}
+        />
+        <Button
+          color="secondary"
+          variant="contained"
+          type="submit"
+          endIcon={<KeyboardArrowRightIcon />}
+        >
+          Submit
+        </Button>
+      </form>
     </Container>
   );
 };
