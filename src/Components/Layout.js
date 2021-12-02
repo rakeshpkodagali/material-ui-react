2af3efd5f283f2b21f0f72d605fe886d18c2ca8e
Rakesh Kodagali <rpk.300@gmail.com>
2021-12-03 00:33:18 +0530

    basic layout drawer

--- src/Components/Layout.js
+++ src/Components/Layout.js
@@ -1,10 +1,23 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core";
+import { Drawer } from "@material-ui/core";
+import { Typography } from "@material-ui/core";
+
+const drawerWidth = "240px";
 
 const useStyles = makeStyles({
   page: {
     backgroundColor: "#f9f9f9",
     width: "100%",
+  },
+  drawer: {
+    width: drawerWidth,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  root: {
+    display: "flex",
   },
 });
 
@@ -12,8 +25,18 @@ const Layout = ({ children }) => {
   const classes = useStyles();
 
   return (
-    <div className={classes.page}>
-      <div>{children}</div>
+    <div className={classes.root}>
+      <Drawer
+        className={classes.drawer}
+        variant="permanent"
+        anchor="left"
+        classes={{ paper: classes.drawerPaper }}
+      >
+        <div>
+          <Typography variant="h5">Ninja Notes</Typography>
+        </div>
+      </Drawer>
+      <div className={classes.page}>{children}</div>
     </div>
   );
 };
